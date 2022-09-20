77152ae59ebd95b0b0b8944e8aad3d46356cd0fa
Taeshawn Threatt <110959584+XP-Tekurai99@users.noreply.github.com>
2022-09-20 11:53:52 -0700

    Update ZombieFactory.sol

--- Solidity Path - Beginner to Intermediate Smart Contracts/Making the Zombie Factory/ZombieFactory.sol
+++ Solidity Path - Beginner to Intermediate Smart Contracts/Making the Zombie Factory/ZombieFactory.sol
@@ -5,7 +5,8 @@ contract ZombieFactory
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
     
-    struct Zombie {
+    struct Zombie
+    {
         string name;
         uint dna;
     }
